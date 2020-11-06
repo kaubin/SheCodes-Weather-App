79ab462a0224e01f30411d3959db9d459e81d4de
kaubin <71940046+kaubin@users.noreply.github.com>
2020-11-06 16:13:04 -0500

    Created index.jss

---
+++ src/index.js
@@ -0,0 +1,38 @@
+let now = new Date();
+let today = document.querySelector("#date");
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+today.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp-celcius").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#conditions").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "f6881838fc1c53a329348e6e031d8cc0";
+  let city = document.querySelector("#city-search").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
