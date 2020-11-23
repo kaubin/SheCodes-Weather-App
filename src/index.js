559d8afd8de1c64a72dd6580c7b478aae5896469
kaubin <71940046+kaubin@users.noreply.github.com>
2020-11-22 22:15:22 -0500

    Forecast created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-let now = new Date();
-let today = document.querySelector("#date");
-let hours = now.getHours();
-let minutes = now.getMinutes();
+function formatDate(timestamp){
+let date = new Date(timestamp);
 let days = [
   "Sunday",
   "Monday",
@@ -11,8 +9,9 @@ let days = [
   "Friday",
   "Saturday"
 ];
-let day = days[now.getDay()];
-today.innerHTML = `${day}, ${hours}:${minutes}`;
+let day = days[date.getDay()];
+return `${day}, ${formatHours(timestamp)}`;
+}
 
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
@@ -20,6 +19,7 @@ function displayWeatherCondition(response) {
     response.data.main.temp
   );
   document.querySelector("#temp-farenheight").innerHTML = Math.round(((response.data.main.temp) * 9) / 5 + 32);
+  document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
@@ -29,13 +29,52 @@ function displayWeatherCondition(response) {
   document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
+function formatHours (timestamp){
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++){
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+     <div class="row">
+        <div class="col-3">${formatHours(forecast.dt * 1000)} 
+        </div>
+        <div class="col-3">
+          <img
+            src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+          />
+        </div>
+        <div class="col-3">high: ${Math.round(forecast.main.temp_max)}°C</div>
+        <div class="col-3">low: ${Math.round(forecast.main.temp_min)}°C</div>
+      </div>`;
+  }
+}
+
 function search(event) {
   event.preventDefault();
   let apiKey = "f6881838fc1c53a329348e6e031d8cc0";
   let city = document.querySelector("#city-search").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
+  
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
-
